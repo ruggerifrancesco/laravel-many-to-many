9002912502b9ed2841e4217db531f12c962aebf7
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-08-28 16:05:08 +0200

    defiend columns for techbnlogies table

--- database/migrations/2023_08_28_135612_create_technologies_table.php
+++ database/migrations/2023_08_28_135612_create_technologies_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('type', 40);
+            $table->string('color', 30)->nullable()->default('000');;
             $table->timestamps();
         });
     }
