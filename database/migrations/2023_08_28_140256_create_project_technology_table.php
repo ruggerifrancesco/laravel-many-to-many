c6aeacf4226de3df963fc4347a145228a9b0574e
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-08-28 18:42:53 +0200

    removed id column causing troubles

--- database/migrations/2023_08_28_140256_create_project_technology_table.php
+++ database/migrations/2023_08_28_140256_create_project_technology_table.php
@@ -12,8 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-
             $table->unsignedBigInteger('project_id');
             $table->foreign('project_id')
                 ->references('id')
