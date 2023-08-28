797b5d27fba6f12ce91a350041034f9ae26be36b
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-08-28 16:14:57 +0200

    defined new technlogies with al key values

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,22 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            ['name' => 'React', 'color' => '#61DAFB', 'type' => 'JavaScript'],
+            ['name' => 'Vue.js', 'color' => '#42b883', 'type' => 'JavaScript'],
+            ['name' => 'Angular', 'color' => '#DD0031', 'type' => 'TypeScript'],
+            ['name' => 'Laravel', 'color' => '#FF2D20', 'type' => 'PHP'],
+            ['name' => 'Node.js', 'color' => '#8CC84B', 'type' => 'JavaScript'],
+            ['name' => 'Django', 'color' => '#092E20', 'type' => 'Python'],
+            ['name' => 'Ruby on Rails', 'color' => '#CC0000', 'type' => 'Ruby'],
+            ['name' => 'Spring Boot', 'color' => '#6DB33F', 'type' => 'Java'],
+            ['name' => 'ASP.NET Core', 'color' => '#512BD4', 'type' => 'C#'],
+            ['name' => 'Flutter', 'color' => '#02569B', 'type' => 'Dart']
+        ];
+
+        foreach ($technologies as $technologiesData) {
+            Technology::create($technologiesData);
+        }
+
     }
 }
