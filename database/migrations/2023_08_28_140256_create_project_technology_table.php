e347b435539d15c0a43c8a4f8280ed6541d10452
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-08-28 16:21:25 +0200

    assigned values for bridge table in order to have vazlues connected

--- database/migrations/2023_08_28_140256_create_project_technology_table.php
+++ database/migrations/2023_08_28_140256_create_project_technology_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')
+                ->references('id')
+                ->on('technologies')
+                ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
